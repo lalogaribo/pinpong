8e19e7c9ab85d663fe0a12d1c96e204e89f7ce7b
lalogaribo <lalo.garibo90@gmail.com>
2018-09-21 18:27:04 -0500

    Added raiting system

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,12 +1,12 @@
+require 'elo'
 class Game < ActiveRecord::Base
   belongs_to :user
   belongs_to :opponent, class_name: 'User'
   validates_presence_of :user_id, :opponent_id
-  validates :user_score, presence: {message: 'Score is needed '},
-            numericality: {greater_than: 0, less_than: 22, message: " Score should be less than 21"}
-  validates :opponent_score, presence: {message: 'Score is needed '},
-            numericality: {greater_than: 0, less_than: 22, message: " Score should be less than 21"}
-  before_save :winner
+  validates :user_score, presence: {message: 'Score is needed '}, numericality: true
+  validates :opponent_score, presence: {message: 'Score is needed '}, numericality: true
+  validate :winner
+  after_save :calculate_rating
 
 
   def result(id)
@@ -19,21 +19,16 @@ class Game < ActiveRecord::Base
   end
 
   def winner
-    true unless user_score > opponent_score && user_score >= 21
-    true unless user_score < opponent_score && opponent_score >= 21
-    else
-    errors.add(:base, "A valid winner with 21 should exist")
-    false
+    errors.add(:base, "Match should have a winner") unless (user_score != opponent_score)
+    errors.add(:base, "Winner score should be 21 or more") unless (user_score >= 21 || opponent_score >= 21)
+    errors.add(:base, "Score should have a difference of 2 or more") unless ((user_score - opponent_score).abs >= 2)
+  end
+
+  def calculate_rating
+    player1 = RatedPlayer.new(user_id, user.score)
+    player2 = RatedPlayer.new(opponent_id, opponent.score)
+    Match.new(player1, player2, user_score > opponent_score ? 1 : 0)
+    user.update_column(:score, player1.rating)
+    opponent.update_column(:score, player2.rating)
   end
 end
-
-# def winner
-#   if user_score >= 21
-#     true if user_score > opponent_score
-#   elsif opponent_score >= 21
-#     true if user_score < opponent_score
-#   end
-# else
-#   errors.add(:base, "A valid winner with 21 should exist")
-#   false
-# end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :trackable, :validatable
   has_many :games
+  has_many :games_challenged, foreign_key: "opponent_id", class_name: "Game"
 
   scope :opponents, ->(user) { where.not(id: user) }
 
   def to_s
     email.split("@").first.titleize
+  end
+
+  def games_played
+    games + games_challenged
   end
 end
